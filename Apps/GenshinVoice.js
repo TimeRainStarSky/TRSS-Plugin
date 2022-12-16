3c356c0ba811840ac75655334213b5112808530f
🌌 <Time.Rain.Star.Sky@Gmail.com>
2022-12-16 19:31:06 +0800

    新增 原神语音合成 API 服务器

--- Apps/GenshinVoice.js
+++ Apps/GenshinVoice.js
@@ -1,8 +1,9 @@
 import { segment } from "oicq"
+import config from "../Model/config.js"
 import { exec } from "child_process"
 import uploadRecord from "../Model/uploadRecord.js"
 
-let path = `${process.cwd()}/plugins/TRSS-Plugin/genshin-voice/`
+let path = `${process.cwd()}/plugins/TRSS-Plugin/GenshinVoice/`
 let Running
 const speakers = ["派蒙", "凯亚", "安柏", "丽莎", "琴", "香菱", "枫原万叶", "迪卢克", "温迪", "可莉", "早柚", "托马", "芭芭拉", "优菈", "云堇", "钟离", "魈", "凝光", "雷电将军", "北斗", "甘雨", "七七", "刻晴", "神里绫华", "戴因斯雷布", "雷泽", "神里绫人", "罗莎莉亚", "阿贝多", "八重神子", "宵宫", "荒泷一斗", "九条裟罗", "夜兰", "珊瑚宫心海", "五郎", "散兵", "女士", "达达利亚", "莫娜", "班尼特", "申鹤", "行秋", "烟绯", "久岐忍", "辛焱", "砂糖", "胡桃", "重云", "菲谢尔", "诺艾尔", "迪奥娜", "鹿野院平藏"]
 let errorTips = "请查看安装使用教程：\nhttps://gitee.com/TimeRainStarSky/TRSS-Plugin\n并将报错通过联系方式反馈给开发者"
@@ -16,7 +17,7 @@ export class GenshinVoice extends plugin {
       priority: 10,
       rule: [
         {
-          reg: `^(${speakers.join("|")})(API)?(转码)?说.+`,
+          reg: `^(${speakers.join("|")})(转码)?说.+`,
           fnc: "GenshinVoice",
         },
       ],
@@ -32,27 +33,6 @@ export class GenshinVoice extends plugin {
   }
 
   async GenshinVoice(e) {
-    let msg = this.e.msg.split("说")
-    let speaker = msg.shift()
-    let data = msg.join("说").replace("'", "").trim()
-
-    let transcoding = false
-    if (speaker.match("转码")) {
-      speaker = speaker.replace("转码", "")
-      transcoding = true
-    }
-
-    if (speaker.match("API")) {
-      let speakerid = speakers.indexOf(speaker.replace("API", ""))
-      let url = `https://genshin.azurewebsites.net/api/speak?format=mp3&id=${speakerid}&text=${encodeURI(data)}`
-      logger.mark(`[原神语音合成] 发送API语音：${logger.blue(url)}`)
-
-      await this.e.reply(
-        await uploadRecord(url, 68714, transcoding)
-      )
-      return true
-    }
-
     if (Running) {
       await this.e.reply("正在生成，请稍等……", true)
       return false
@@ -60,21 +40,37 @@ export class GenshinVoice extends plugin {
     Running = true
     await this.e.reply("开始生成，请稍等……", true)
 
-    let cmd = `sh ${path}main.sh output.wav '${speaker}' '${data}'`
+    let msg = this.e.msg.split("说")
+    let speaker = msg.shift()
+    let speakerid = speakers.indexOf(speaker)
+    let text = msg.join("说").replace("'", "").trim()
 
-    logger.mark(`[原神语音合成] 执行：${logger.blue(cmd)}`)
-    let ret = await this.execSync(cmd)
-    logger.mark(`[原神语音合成]\n${ret.stdout.trim()}\n${logger.red(ret.stderr.trim())}`)
-
-    if (ret.error) {
-      logger.error(`原神语音合成错误：${logger.red(ret.error)}`)
-      await this.e.reply(`原神语音合成错误：${ret.error}`, true)
-      await this.e.reply(errorTips)
+    let transcoding = false
+    if (speaker.match("转码")) {
+      speaker = speaker.replace("转码", "")
+      transcoding = true
     }
 
-    await this.e.reply(
-      await uploadRecord(`${path}output.wav`, 68714, transcoding)
-    )
+    let url
+    if (config.GenshinVoice.api) {
+      url = `${config.GenshinVoice.api}?id=${speakerid}&text=${encodeURIComponent(text)}`
+    } else {
+      let cmd = `sh ${path}main.sh output.wav ${speakerid} '${text}'`
+
+      logger.mark(`[原神语音合成] 执行：${logger.blue(cmd)}`)
+      let ret = await this.execSync(cmd)
+      logger.mark(`[原神语音合成]\n${ret.stdout.trim()}\n${logger.red(ret.stderr.trim())}`)
+
+      if (ret.error) {
+        logger.error(`原神语音合成错误：${logger.red(ret.error)}`)
+        await this.e.reply(`原神语音合成错误：${ret.error}`, true)
+        await this.e.reply(errorTips)
+      }
+      url = `${path}output.wav`
+    }
+
+    logger.mark(`[原神语音合成] 发送语音：${logger.blue(url)}`)
+    await this.e.reply(await uploadRecord(url, 68714, transcoding))
     Running = false
   }
 }
--- README.md
+++ README.md
@@ -48,7 +48,7 @@ poetry run python setup.py develop
 poetry run pip install flask
 
 # 启动 API 服务器
-poetry run python inference_realesrgan_server.py
+poetry run python inference_realesrgan_server.py [端口]
 ```
 
 </details>
@@ -56,13 +56,25 @@ poetry run python inference_realesrgan_server.py
 - 原神语音合成：
 
 ```
-git clone --depth=1 https://gitee.com/TimeRainStarSky/genshin-voice
-cd genshin-voice
+git clone --depth=1 https://gitee.com/TimeRainStarSky/GenshinVoice
+cd GenshinVoice
 poetry install
 poetry run pip install monotonic-align
 curl -LO https://github.com/TimeRainStarSky/TRSS-Plugin/releases/download/latest/G_809000.pth.xz
 xz -dv G_809000.pth.xz
 ```
+
+<details><summary>部署为 API 服务器</summary>
+
+```
+# 安装 Flask
+poetry run pip install flask
+
+# 启动 API 服务器
+poetry run python server.py [端口]
+```
+
+</details>
 
 - 阿里云盘 / 百度网盘：
 
@@ -86,7 +98,7 @@ xz -dv G_809000.pth.xz
 
 <details><summary>展开</summary>
 
-- `角色名` + (API)?(转码)?说 + `中文`
+- `角色名` + (转码)?说 + `中文`
 - 支持角色：派蒙、凯亚、安柏、丽莎、琴、香菱、枫原万叶、迪卢克、温迪、可莉、早柚、托马、芭芭拉、优菈、云堇、钟离、魈、凝光、雷电将军、北斗、甘雨、七七、刻晴、神里绫华、戴因斯雷布、雷泽、神里绫人、罗莎莉亚、阿贝多、八重神子、宵宫、荒泷一斗、九条裟罗、夜兰、珊瑚宫心海、五郎、散兵、女士、达达利亚、莫娜、班尼特、申鹤、行秋、烟绯、久岐忍、辛焱、砂糖、胡桃、重云、菲谢尔、诺艾尔、迪奥娜、鹿野院平藏
 
 </details>
