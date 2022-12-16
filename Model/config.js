5026b673324eead2d708c79ab47a14cb37963f9e
🌌 <Time.Rain.Star.Sky@Gmail.com>
2022-12-16 20:18:24 +0800

    优化 读取配置文件

--- Model/config.js
+++ Model/config.js
@@ -1,13 +1,38 @@
 import fs from "fs"
 import YAML from "yaml"
+import _ from "lodash"
 
 let path = `${process.cwd()}/plugins/TRSS-Plugin/`
 let configFile = `${path}config.yaml`
+let configData
 
-if (!fs.existsSync(configFile)) {
-  fs.copyFileSync(`${path}config_demo.yaml`, configFile)
+let config = {
+  tips: ["欢迎使用 TRSS Yunzai Plugin ! 作者：时雨🌌星空", "按 Ctrl+Q Y 保存退出", "参考：https://gitee.com/TimeRainStarSky/TRSS-Plugin"],
+
+  GenshinVoice: {
+    api: ""
+  },
+
+  RealESRGAN: {
+    api: ""
+  },
+
+  ChatGPT: {
+    api: "",
+    tokenapi: "",
+    username: "",
+    password: "",
+    sessionToken: ""
+  }
 }
 
-let config = YAML.parse(fs.readFileSync(configFile, "utf-8"))
+if (fs.existsSync(configFile)) {
+  configData = YAML.parse(fs.readFileSync(configFile, "utf-8"))
+  _.merge(config, configData)
+}
+
+if (config != configData) {
+  fs.writeFileSync(configFile, YAML.stringify(config), "utf-8")
+}
 
 export default config
